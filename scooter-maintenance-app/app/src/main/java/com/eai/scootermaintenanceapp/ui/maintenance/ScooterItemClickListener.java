4c835f10ed3437d0ef5813c640fe42e74e5425c2
rriesebos <rriesebos@hotmail.com>
2020-12-17 21:32:56 +0100

    Implemented scooter list, scooters are managed by the viewmodel and a single scooter can be selected

---
+++ scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/ui/maintenance/ScooterItemClickListener.java
@@ -0,0 +1,7 @@
+package com.eai.scootermaintenanceapp.ui.maintenance;
+
+import com.eai.scootermaintenanceapp.data.model.Scooter;
+
+public interface ScooterItemClickListener {
+    void onScooterItemClick(Scooter scooter);
+}
--- scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/ui/maintenance/scooterlist/ScooterListFragment.java
+++ scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/ui/maintenance/scooterlist/ScooterListFragment.java
@@ -5,19 +5,56 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.eai.scootermaintenanceapp.R;
+import com.eai.scootermaintenanceapp.data.model.Scooter;
+import com.eai.scootermaintenanceapp.ui.maintenance.ScooterItemClickListener;
+import com.eai.scootermaintenanceapp.ui.maintenance.ScooterViewModel;
 
-public class ScooterListFragment extends Fragment {
+public class ScooterListFragment extends Fragment implements ScooterItemClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private ScooterViewModel mScooterViewModel;
+    private ScooterListAdapter mScooterListAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_scooter_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_scooter_list, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.scooter_list);
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        mScooterListAdapter = new ScooterListAdapter(this);
+        recyclerView.setAdapter(mScooterListAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mScooterViewModel = new ViewModelProvider(requireActivity()).get(ScooterViewModel.class);
+        mScooterViewModel.getScooterList().observe(getViewLifecycleOwner(), scooterList -> {
+            mScooterListAdapter.updateScooterList(scooterList);
+        });
+
+        mScooterViewModel.getSelectedScooter().observe(getViewLifecycleOwner(), selectedScooter -> {
+            mScooterListAdapter.setSelectedScooter(selectedScooter);
+        });
+    }
+
+    @Override
+    public void onScooterItemClick(Scooter scooter) {
+        mScooterViewModel.selectScooter(scooter);
     }
 }
