1f96a8c1de2ea36531b9aa93e59f86d9e88f1330
rriesebos <rriesebos@hotmail.com>
2020-12-18 15:47:50 +0100

    Added logout functionality

--- scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/ui/maintenance/MaintenanceActivity.java
+++ scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/ui/maintenance/MaintenanceActivity.java
@@ -2,6 +2,7 @@ package com.eai.scootermaintenanceapp.ui.maintenance;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -9,28 +10,53 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.eai.scootermaintenanceapp.R;
+import com.eai.scootermaintenanceapp.ui.login.LoginViewModel;
+import com.eai.scootermaintenanceapp.ui.login.LoginViewModelFactory;
 import com.eai.scootermaintenanceapp.util.BottomNavigationPosition;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MaintenanceActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private static final String LOG_TAG = MaintenanceActivity.class.getSimpleName();
-
     private static final String KEY_ITEM_ID = "keyItemId";
 
     private Integer mNavItemId = BottomNavigationPosition.SCOOTER_LIST.itemId;
+
+    private LoginViewModel mLoginViewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance);
 
+        mLoginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         initFragment(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            mLoginViewModel.logout();
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
