69036a3873281cec7f136ea09dfd6287792ca9a9
rriesebos <rriesebos@hotmail.com>
2021-01-07 23:10:23 +0100

    Implemented producer, some refactoring

--- scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/messaging/ScooterProducer.java
+++ scooter-maintenance-app/app/src/main/java/com/eai/scootermaintenanceapp/messaging/ScooterProducer.java
@@ -3,6 +3,7 @@ package com.eai.scootermaintenanceapp.messaging;
 import com.eai.scootermaintenanceapp.data.model.Region;
 import com.eai.scootermaintenanceapp.data.model.Scooter;
 import com.swiftmq.amqp.AMQPContext;
+import com.swiftmq.amqp.v100.client.AMQPException;
 import com.swiftmq.amqp.v100.client.Connection;
 import com.swiftmq.amqp.v100.client.ExceptionListener;
 import com.swiftmq.amqp.v100.client.Producer;
@@ -81,18 +82,68 @@ public class ScooterProducer {
     }
 
     public void produce(Scooter scooter) {
-        // TODO: implement
-    }
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    AMQPContext ctx = new AMQPContext(AMQPContext.CLIENT);
 
-    public void stopProducing() {
-        isProducing = false;
+                    // Authentication is disabled because required imports for the constructor below
+                    //     new Connection(ctx, hostName, port, "default", "default");
+                    // gave java.lang.ClassNotFoundException errors due to differences between
+                    // the Java Virtual Machine (JVM) and the Dalvik Virtual Machine (DVM) that
+                    // Android uses (javax is not included).
+                    if (connection == null) {
+                        connection = new Connection(ctx, hostName, port, false);
+                        connection.setContainerId("maintenanceAppProducer");
+                        connection.setExceptionListener(new ExceptionListener() {
+                            public void onException(Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+
+                        connection.connect();
+                    }
+
+                    if (session == null) {
+                        session = connection.createSession(50, 50);
+                    }
+
+                    if (producer == null) {
+                        producer = session.createProducer("TEST.FOO", QoS.AT_LEAST_ONCE);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                AMQPMessage message = new AMQPMessage();
+                String jsonString = MessagingMapper.scooterToJson(scooter);
+                message.setAmqpValue(new AmqpValue(new AMQPString(jsonString)));
+
+                try {
+                    producer.send(message);
+                } catch (AMQPException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 
     public void close() {
         try {
-            producer.close();
-            session.close();
-            connection.close();
+            if (producer != null) {
+                producer.close();
+            }
+
+            if (session != null) {
+                session.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
